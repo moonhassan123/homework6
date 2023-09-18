42ddd003438efb9112383f5b374e04af1fb73b7d
moonhassan123 <moonhassan9899@gmail.com>
2023-09-19 00:46:39 +0600

    all complet

---
+++ basic.js
@@ -0,0 +1,106 @@
+var num1 = 10;
+var num2 = 20;
+// console.log(num1 + num2);
+
+var num1 = 10;
+var num2 = 20;
+var sum = num1 + num2;
+var subtraction
+// console.log('sum =', sum);
+// console.log(sum);
+let student1 = 'mehedi'
+student1 = 'hasan'
+const student2 = 'moon'
+
+student1 = 'mehedi'
+// student2 = 'joy'
+
+// console.log(student2);
+let number1 = 20; 
+let number2 = 30;
+// let sum = number1 + number2;
+let sub = number2 - number1
+let multiplication = number1 * number2;
+let divition = number1 / number2;
+let remainer = number1 % number2;
+// console.log('sum =', sum); 
+// console.log('sub =', sub); 
+// console.log('multiplication =', multiplication); 
+// console.log('divition =', divition); 
+// console.log('remainer =', remainer); 
+
+let score_board = 200
+score_board += 4
+
+
+
+score_board =score_board + 5;
+score_board =score_board + 6;
+// console.log('score_board =', score_board);
+let bikasAccount = 2000;
+bikasAccount = bikasAccount + 1000;
+bikasAccount += 500;
+bikasAccount -= 500;
+bikasAccount = 500;
+
+// console.log('bikasAccount =', bikasAccount);
+
+let counter = 1;
+ counter++;
+ counter++;
+ counter--;
+//  console.log(counter);
+
+
+let firstName = 'mehedi'
+let lastName = 'hasan'
+let fullName = firstName + " " + lastName;
+// console.log(fullName);
+let value1 = 20;
+let value2 = '30'
+let value2_convert_in_number = parseInt(value2)
+
+
+let totel = value1 + value2_convert_in_number;
+// console.log(totel);
+let  a = 0.1;
+let b = 0.2;
+let sum1 = a + b;
+// console.log(sum1);
+let convertfixed = sum1.toFixed(1)
+// console.log( "convert in one decimal",convertfixed);
+let convert_in_numberr = parseFloat(convertfixed)
+
+let convert_in_tow_decimal_number =
+// console.log('convert in number',  convert_in_numberr);
+// console.log(value1);
+
+
+// if else comdition
+// let a1= 20;
+// let a2= 30;
+
+let pass1= 123456;
+let user= "moonhassan9899@gmail.com"
+
+// login page
+
+if (123456 == pass1 && "moonhassan9899@gmail.com" == user) {
+    console.log( 'showing you your profill');
+}
+
+else{
+    console.log('pass1 wrong');
+}
+
+
+
+
+
+
+
+
+
+
+
+
